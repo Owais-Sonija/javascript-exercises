19d534278c68354be84eab28cf89bfc7500a2e55
Owais-Sonija <owaissonija007@gmail.com>
2024-08-28 14:26:34 +0500

    Completed Exercise 10 Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
+let index = parseInt(idx);
+if (index <0) {
+    return "OOPS"
+}
+    const series = [0,1 ];
+for (let i = 0; i < 100; i++) {
+    let lastNum = series[series.length -1];
+    let secondLastNum = series[series.length -2];
+    let sum = lastNum + secondLastNum;
+    series.push(sum)    
+}
 
+return series[index];
 };
 
 // Do not edit below this line
